f348c5750656335f38d3ca744c04f3620c22dacf
Mack <gayonmarkanthony@gmail.com>
2018-12-09 17:22:03 -0800

    Email

--- config.py
+++ config.py
@@ -12,5 +12,5 @@ class Config(object):
     MAIL_USES_TLS = os.environ.get('MAIL_USES_TLS') is not None
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    ADMINS = ['your-email@example.com']
+    ADMINS = ['gayonmarkanthony@gmail.com']
     POSTS_PER_PAGE = 25
